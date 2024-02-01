5e7a936ec8650835c41be85ff0b827905d2410b1
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2024-02-01 06:57:11 +0000

    Edited transfer.transaction_history.ItemTemplate5

--- client_code/transfer/transaction_history/ItemTemplate5/__init__.py
+++ client_code/transfer/transaction_history/ItemTemplate5/__init__.py
@@ -10,5 +10,5 @@ class ItemTemplate5(ItemTemplate5Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
